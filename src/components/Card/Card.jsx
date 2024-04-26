96f96250e4b02af701adc131a3383f93056cbf9a
Chas3Pap3rs <futurefineants@gmail.com>
2024-04-26 15:44:20 -0400

    Disabled some edit features on cards/tasks that are isComplete true

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -30,17 +30,33 @@ const Card = ({ taskObj, index, deleteTask, updateListArray, handleSetCompleted 
     handleSetCompleted(index); // Call the prop function to update completed status in parent component
   };
 
+    // Define a function to handle click on card header
+    const handleCardHeaderClick = () => {
+      // Do nothing when the card is completed
+      if (!completed) {
+        setModal(true);
+      }
+    };
+  
+    // Define a function to handle click on card details
+    const handleCardDetailsClick = () => {
+      // Do nothing when the card is completed
+      if (!completed) {
+        setModal(true);
+      }
+    };
+
   return (
     <>
       <div key={index} className={`card-wrapper mr-5 ${completed ? 'completed' : ''}`} style={{ "position": "relative", "backgroundColor": "#252524", "filter": completed ? "brightness(40%)" : "none" }}>
         <div className="card-top" style={{ "backgroundColor": "#252524" }}>
-          <span className="card-header" style={{ "backgroundColor": "#252524", "borderRadius": "10px", "cursor": "pointer" }} onClick={() => setModal(true)}>{taskObj.Name}</span>
-          <i className="fa-solid fa-pencil" style={{ "color": "white", "cursor": "pointer" }} onClick={() => setModal(true)}></i>
+          <span className={`card-header ${completed ? 'disable-pointer' : ''}`} style={{ "backgroundColor": "#252524", "borderRadius": "10px", "cursor": "pointer" }} onClick={handleCardHeaderClick}>{taskObj.Name}</span>
+          {!completed && <i className="fa-solid fa-pencil" style={{ "color": "white", "cursor": "pointer" }} onClick={() => setModal(true)}></i>}
           <i className="fa-solid fa-crosshairs" style={{ "color": "white", "cursor": "pointer" }} onClick={handleCompletedClick}></i>
           <i className="fa-solid fa-toilet ml-2" style={{ "cursor": "pointer", "justifyContent": "flex-end" }} onClick={handleDelete}></i>
         </div>
         <div className="task-holder overflow-auto">
-          <p className="card-details mt-3" onClick={() => setModal(true)}>{taskObj.Description}</p>
+          <p className={`card-details mt-3 ${completed ? 'disable-scroll' : ''}`} style={{"cursor": "crosshair"}} onClick={handleCardDetailsClick}>{taskObj.Description}</p>
         </div>
         <EditTask modal={modal} toggle={toggle} updateTask={updateTask} taskObj={taskObj} />
       </div>
